fc2ddd6c96e029fae4fef66c3e0440916b8a534a
Roman Havryliuk <gavru4r@gmail.com>
2022-01-14 22:13:11 +0200

    finish task

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,16 +1,13 @@
 
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+ const BASE_URL = 'https://restcountries.com/v3.1/name/';
 
 export default function fetchCountries(name) {
-  const BASE_URL = 'https://restcountries.com/v3.1/name/';
-  return fetch(`${BASE_URL} ${name}?fields=name,capital,currencies,population,flags,languages`)
+  return fetch(`${BASE_URL}${name}?fields=name,capital,currencies,population,flags,languages`)
     .then(response => {
       if (!response.ok) {
-        throw new Error( response.status);
+        throw new Error(response.status);
       }
       return response.json();
-    })
-    // .catch(error => {
-    //   Notify.failure("Oops, there is no country with that name");
-    // })
-}
+    }
+    );
+} 
--- src/index.js
+++ src/index.js
@@ -6,29 +6,31 @@ import _ from 'lodash';
 
 
 const DEBOUNCE_DELAY = 300;
-// Notify.info('Cogito ergo sum');
+
 
 const input = document.querySelector("#search-box")
 const countryList = document.querySelector(".country-list")
 const countryInfo = document.querySelector(".country-info")
-let markup = null;
-input.addEventListener('input', debounce(getCountry, 500));
+
+input.addEventListener('input', debounce(getCountry, DEBOUNCE_DELAY));
   
 function getCountry() {
     const inputValue = input.value.trim();
-console.log(input.value=== '');
-    if (!inputValue) return;
+
+    if (!inputValue) {
+        countryList.innerHTML = "";
+        countryInfo.innerHTML = "";
+        return;
+    }
 
     fetchCountries(inputValue).then(data => {
         if (data.length === 1) {
-             
-            renderMarkup(data);
-              countryList.innerHTML = "";
+             countryList.innerHTML = '';
              renderCountryInfo(data);
-           
-         }
+        }
         if (data.length <= 10 && data.length > 1) {
-        countryList.innerHTML = "";
+            countryInfo.innerHTML = ""
+            countryList.innerHTML = "";
             renderMarkup(data);
            
         }
@@ -44,27 +46,33 @@ console.log(input.value=== '');
  
 
 function renderMarkup(data) {
-      countryList.innerHTML = "";
-    const markup = data.map(item => {
-          return `<li class="contry-list"><img src="${item.flags.svg}" width ="100"><p class ="country-name">${item.name.common}</p></li>`
-    }).join() 
-    countryList.insertAdjacentHTML('afterbegin', markup);
+    countryList.innerHTML = '';
+    const markup = data
+        .map(({flags, name}) => {
+        return`<li class="contry-list">
+                    <img src="${flags.svg}" alt="img" width="100">
+                    <p class="country-name">${name.common}</p>
+                </li>`}).join('');
+    countryList.innerHTML = markup;
 }
 
 function renderCountryInfo(data) {
-    const countryDescr = data.map(({ capital, population, languages, flags, name }) => {
-        // return `<li>
-        //         <img src="${flags.svg}" width ="100">
-        //         <p class ="country-name">${name.common}</p>
-            return    `<ul>
-                    <li>Capital:<span>${capital}</span></li>
-                    <li>Population:<span>${population}</span></li>
-                    <li>Languages:<span>${Object.values(languages).join(',')}</span></li>
-                </ul>
-        </li>`
-    }).join()
+      countryList.innerHTML = '';
+    const countryDescr = data
+        .map(({ capital, population, languages, flags, name }) => {
+            return `<div class ="wrapper">
+                <img src="${flags.svg}" alt="img" width="100">
+                <h1>${name.common}</h1></div>
+            <ul class="country-description">
+                <li class="contry-list">
+                    <p><b>Capital:</b>${capital}</p>
+                    <p><b>Population:</b>${population}</p>
+                    <p><b>Languages:</b>${Object.values(languages).join(',')}</p>
+                </li>
+            </ul>`;
+    }).join();
 
-    countryInfo.insertAdjacentHTML("afterbegin",countryDescr);
+    countryInfo.innerHTML = countryDescr;
 }
 
 
